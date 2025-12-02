e31ef1726320eb28514a2a79f905979aed794f19
Jacopo Marrone <jacopo.marrone27@gmail.com>
2025-12-02 14:06:12 +0100

    feat: update home page UI

--- frontend-react/src/routes/(home)/index.lazy.tsx
+++ frontend-react/src/routes/(home)/index.lazy.tsx
@@ -1,9 +1,17 @@
 import { createLazyFileRoute } from '@tanstack/react-router';
 
+import { SectionHeader } from './-components/section-header';
+import { SectionCookieEditor } from './-components/section-cookie-editor';
+
 export const Route = createLazyFileRoute('/(home)/')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Hello "/"!</div>;
+  return (
+    <>
+      <SectionHeader />
+      <SectionCookieEditor />
+    </>
+  );
 }
